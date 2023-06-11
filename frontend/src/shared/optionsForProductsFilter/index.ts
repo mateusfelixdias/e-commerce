fe01675288dd38f64f27d1629093ed9eaba441bd
mateusfelixdias <felixmateus618@gmail.com>
2023-06-11 14:48:33 -0300

    feat: added options for products filter and options order by

---
+++ frontend/src/shared/optionsForProductsFilter/index.ts
@@ -0,0 +1,7 @@
+const options = [
+  { label: 'TODOS OS PRODUTOS', value: '' },
+  { label: 'CAMISETAS', value: 't-shirts' },
+  { label: 'CANECAS', value: 'mugs' },
+];
+
+export default options;
---
+++ frontend/src/shared/optionsOrderBy/index.ts
@@ -0,0 +1,8 @@
+const optionsOrderBy = [
+  { label: 'Novidades', value: 1 },
+  { label: 'Preço: Maior - menor', value: 2 },
+  { label: 'Preço: Menor - maior', value: 3 },
+  { label: 'Mais vendidos', value: 4 },
+];
+
+export default optionsOrderBy;
