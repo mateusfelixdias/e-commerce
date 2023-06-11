b6f80619d5dcc2060f7dee93bf42f92c87324908
mateusfelixdias <felixmateus618@gmail.com>
2023-06-11 15:50:41 -0300

    feat: added pagination

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,52 @@
+'use client';
+
 import styles from './page.module.css';
 import Headers from '../components/Headers';
+import Pagination from '../components/Pagination';
+import CardProduct from '../components/CardProduct';
+import useProductsContext from '../context/ProductsContext';
+import ProductFiltersAndPagination from '../components/ProductFiltersAndPagination';
 
 export default function Home() {
+  const { itemsCurrentPage } = useProductsContext();
+
+  const renderItemsCurrentPage = () => {
+    const items = itemsCurrentPage.map((item) => {
+      const { id, image_url, name, price_in_cents } = item;
+
+      return (
+        <CardProduct
+          id={id}
+          key={id}
+          name={name}
+          imageUrl={image_url}
+          price={price_in_cents}
+        />
+      );
+    });
+
+    return items;
+  };
+
   return (
-    <main className={styles.main}>
-      <Headers />
-    </main>
+    <>
+      <header className={styles.header}>
+        <Headers />
+      </header>
+
+      <main className={styles.main}>
+        <ProductFiltersAndPagination />
+
+        <div>
+          <div className={styles.cardProductsContainer}>
+            {renderItemsCurrentPage()}
+          </div>
+
+          <div className={styles.pagination}>
+            <Pagination />
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
