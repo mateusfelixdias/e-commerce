fde21856de344e42f40d509070960e7afea4f61b
mateusfelixdias <felixmateus618@gmail.com>
2023-06-11 15:31:20 -0300

    feat: added option for search at headear

--- frontend/src/components/Headers/index.tsx
+++ frontend/src/components/Headers/index.tsx
@@ -1,11 +1,46 @@
-import { HeaderMobile } from '../HeaderMobile';
-import { HeaderDesktop } from '../HeaderDesktop';
+'use client';
+
+import HeaderMobile from '../HeaderMobile';
+import { useEffect, useState } from 'react';
+import { HeadersContainer } from './styles';
+import HeaderDesktop from '../HeaderDesktop';
+import useProductsContext from '../../context/ProductsContext';
 
 export default function Headers() {
+  const { products, setProductsFiltered } = useProductsContext();
+
+  const [keyword, setKeyword] = useState('');
+
+  const handleSearch = () => {
+    if (!keyword.length) return;
+
+    const filterProducts = products.filter(({ name }) => {
+      const newName = name.toLowerCase();
+      const newKeyword = keyword.trim().toLowerCase();
+
+      return newName.includes(newKeyword);
+    });
+
+    setProductsFiltered(filterProducts);
+  };
+
+  useEffect(() => {
+    setProductsFiltered(products);
+  }, [keyword.length === 0]);
+
   return (
-    <>
-      <HeaderDesktop />
-      <HeaderMobile />
-    </>
+    <HeadersContainer>
+      <HeaderDesktop
+        keyword={keyword}
+        onKeyword={setKeyword}
+        onSearch={handleSearch}
+      />
+
+      <HeaderMobile
+        keyword={keyword}
+        onKeyword={setKeyword}
+        onSearch={handleSearch}
+      />
+    </HeadersContainer>
   );
 }
