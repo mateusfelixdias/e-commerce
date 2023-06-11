8afc70989892aba7cb5176e826707a0989abf469
mateusfelixdias <felixmateus618@gmail.com>
2023-06-11 15:46:08 -0300

    feat: added product provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import { Saira } from 'next/font/google';
+import { ProductsProvider } from '../context/ProductsContext';
 
 import './globals.css';
 
-const saira = Saira({ weight: ['300', '400', '500', '600'], subsets: ['latin'] });
+const saira = Saira({
+  weight: ['300', '400', '500', '600'],
+  subsets: ['latin-ext'],
+});
 
 export const metadata = {
   title: 'Capputeeno',
@@ -11,8 +15,10 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR">
-      <body className={saira.className}>{children}</body>
+    <html lang="pt-BR" className={saira.className}>
+      <ProductsProvider>
+        <body>{children}</body>
+      </ProductsProvider>
     </html>
   );
 }
