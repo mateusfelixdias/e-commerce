822f412b9d42aa23486f6e9102978dcb709b9cd9
mateusfelixdias <felixmateus618@gmail.com>
2023-06-11 15:32:51 -0300

    styles: added responsiveness

---
+++ frontend/src/components/Headers/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const HeadersContainer = styled.div`
+  margin: 0 10rem;
+
+  @media (max-width: 1100px) {
+    margin: 0 5rem;
+  }
+
+  @media (max-width: 900px) {
+    margin: 0 2.5rem;
+  }
+
+  @media (max-width: 360px) {
+    margin: 0 0.5rem;
+  }
+`;
