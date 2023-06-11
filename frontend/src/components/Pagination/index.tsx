39ec22163bd49c61705548adbc85b5d9ce1f3940
mateusfelixdias <felixmateus618@gmail.com>
2023-06-11 15:36:33 -0300

    feat: added pagination

---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import Button from './steps/Button';
+import { useEffect, useState } from 'react';
+import BackAndNextButton from './steps/BackAndNextButton';
+import { ButtonsContainer, Container, PoteDots } from './styles';
+import useProductsContext from '../../context/ProductsContext';
+
+export default function Pagination() {
+  const { productsFiltered, products, setItemsCurrentPage } = useProductsContext();
+
+  const data = productsFiltered.length ? productsFiltered : products;
+
+  const [items, setItems] = useState([...data]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const itemsByPage = 12;
+  const LimitSampleButtons = 5;
+  const totalItems = items.length;
+
+  const totalPages = Math.ceil(totalItems / itemsByPage);
+
+  let startInterval = Math.max(1, currentPage - Math.floor(LimitSampleButtons / 2));
+  let endInterval = Math.min(startInterval + LimitSampleButtons - 1, totalPages);
+
+  startInterval = Math.max(1, endInterval - LimitSampleButtons + 1);
+
+  const goToPage = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const calculateItemsCurrentPage = () => {
+    const initialIndex = (currentPage - 1) * itemsByPage;
+    const endIndex = initialIndex + itemsByPage;
+
+    return items.slice(initialIndex, endIndex);
+  };
+
+  const renderButtonsPagination = () => {
+    const buttons = [];
+
+    const PoteDotsOne = () => <PoteDots key="pote-dots-one">...</PoteDots>;
+    const FirstButton = () => (
+      <Button
+        key={1}
+        currentIndex={1}
+        currentPage={currentPage}
+        onGoToPage={() => goToPage(1)}
+      >
+        1
+      </Button>
+    );
+
+    if (startInterval > 1) buttons.push(<FirstButton />);
+    if (startInterval > 2) buttons.push(<PoteDotsOne />);
+
+    for (let pageNumber = startInterval; pageNumber <= endInterval; pageNumber++) {
+      buttons.push(
+        <Button
+          key={pageNumber}
+          currentPage={currentPage}
+          currentIndex={pageNumber}
+          onGoToPage={() => goToPage(pageNumber)}
+        >
+          {pageNumber}
+        </Button>
+      );
+    }
+
+    const PoteDotsTwo = () => <PoteDots key="pote-dots-two">...</PoteDots>;
+    const LastButton = () => (
+      <Button
+        key={totalPages}
+        currentIndex={totalPages}
+        currentPage={currentPage}
+        onGoToPage={() => goToPage(totalPages)}
+      >
+        {totalPages}
+      </Button>
+    );
+
+    if (endInterval < totalPages) buttons.push(<LastButton />);
+    if (endInterval < totalPages - 1) buttons.push(<PoteDotsTwo />);
+
+    return buttons;
+  };
+
+  useEffect(() => {
+    setItems([...data]);
+  }, [data]);
+
+  useEffect(() => {
+    const items = calculateItemsCurrentPage();
+    setItemsCurrentPage(items);
+  }, [currentPage, items]);
+
+  return (
+    <Container>
+      <ButtonsContainer>{renderButtonsPagination()}</ButtonsContainer>
+
+      <BackAndNextButton
+        onGoToPage={goToPage}
+        totalPages={totalPages}
+        currentPage={currentPage}
+      />
+    </Container>
+  );
+}
---
+++ frontend/src/components/Pagination/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  gap: 0.5rem;
+  display: flex;
+  margin-top: 2rem;
+  align-items: center;
+`;
+
+export const ButtonsContainer = styled.div`
+  gap: 0.125rem;
+  display: flex;
+`;
+
+export const PoteDots = styled.span`
+  padding-top: 1rem;
+  color: var(--dark-grey);
+`;
